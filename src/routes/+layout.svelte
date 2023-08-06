2e713d3583a3ef121b9170fc589cda812f0f4593
Arty <atyinthavongsa@gmail.com>
2023-08-06 15:54:33 +0700

    🐳 chore(routing): add required pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+	import '../app.css';
+	import CodeGeekLogo from '$lib/assets/codegeek-icon-alt.webp';
+	import "@fontsource/silkscreen"
+</script>
+
+<nav class="hidden md:flex flex-row justify-between items-center mx-16">
+	<span class="inline-flex items-center">
+		<a href="/" class="pr-12">
+			<img src={CodeGeekLogo} alt="CodeGeek logo" class="w-20" />
+		</a>
+	</span>
+	<a id="artyboy" href="https://artyboy.dev">artyboy.dev</a>
+</nav>
+<nav class="flex md:hidden flex-row justify-center items-center mx-4">
+	<a href="/">
+		<img src={CodeGeekLogo} alt="CodeGeek logo" class="w-20" />
+	</a>
+</nav>
+<main class="mx-16 my-8">
+	<slot />
+</main>
+
+<style scoped>
+	#artyboy {
+		font-family: "Silkscreen", sans-serif;
+	}
+</style>
---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Our Products</h1>
+<!-- TODO: build all products page  -->
+<p><a href="/products/hello-stranger">Product 1</a></p>
